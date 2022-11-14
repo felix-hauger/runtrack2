547ae8fd674a91a901dc90a5177617ef778eac41
felix-hauger <felix.hauger@laplateforme.io>
2022-11-14 16:04:52 +0100

    jour01, job01 - affiché nombres

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,11 +1,29 @@
 <?php
 
+// strings
 $str = "LaPlateforme";
 echo $str;
 
 $str2 = 'Vive';
 $str3 = '!';
 
+// sauts de lignes
 echo '<br>';
 
 echo "$str2 $str $str3";
+
+
+
+echo '<br><br>';
+
+// ints
+
+$val = 6;
+
+echo $val;
+
+echo '<br>';
+
+$val = $val + 4;
+
+echo $val;
