0decb8eeb0249b6c0d2f0d425952d207df6ef162
felix-hauger <felix.hauger@laplateforme.io>
2022-11-17 18:06:15 +0100

    jour04, job01 - tous champs requis en html/php

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -1,27 +1,33 @@
 <?php
 
-$count = 0;
+echo '<form action="index.php" method="get">
+<label for="mytext">Mon texte</label>
+<input type="text" id="mytext" name="mytext" required>
 
-foreach($_GET as $input) {
-    $count+=1;
+<label for="mytext2">Mon texte 2</label>
+<input type="text" id="mytext2" name="mytext2" required>
+
+<label for="myprettytext">Mon joli texte</label>
+<input type="text" id="myprettytext" name="myprettytext" required>
+
+<input type="submit" value="Envoyer">
+</form>';
+
+
+if (empty($_GET["mytext"]) || empty($_GET["mytext2"]) || empty($_GET["myprettytext"])) {
+    echo 'Remplissez tous les champs !';
+} else {
+    $count = 0;
+
+    foreach($_GET as $input) {
+        $count+=1;
+    }
+    
+    echo 'Le nombre d’argument GET envoyé est : ' . $count;
 }
 
-echo 'Le nombre d’argument GET envoyé est : ' . $count;
+
 
 ?>
 
 
-<form action="index.php" method="get">
-    <label for="mytext">Mon texte</label>
-    <input type="text" id="mytext" name="mytext">
-
-    <label for="mytext2">Mon texte 2</label>
-    <input type="text" id="mytext2" name="mytext2">
-
-    <label for="myprettytext">Mon joli texte</label>
-    <input type="text" id="myprettytext" name="myprettytext">
-
-
-
-    <input type="submit" value="Envoyer">
-</form>
