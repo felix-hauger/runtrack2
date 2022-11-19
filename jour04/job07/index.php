ca3db77b320bfc03f661189e313d59f7e0007123
felix-hauger <felix.hauger@laplateforme.io>
2022-11-19 10:02:41 +0100

    jour04, job07 - tous les champs doivent être remplis & envoyés

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -10,115 +10,116 @@
     }
 </style>
 
-<form action="" method="get">
-    <label for="height"></label>
-    <input type="number" name="height" id="height">
-    
-    <label for="width"></label>
-    <input type="number" name="width" id="width">
-
-    <input type="submit" value="Bâtissez votre maison!">
-</form>
 
 <?php
 
-// if (!isset($_GET['height']) && !isset($_GET['width'])) {
-//     echo 'Remplissez tous les champs !';
-// } else {
+echo '<form action="" method="get">
+<label for="height"></label>
+<input type="number" name="height" id="height">
 
-// }
-$nb_width = $_GET['width'];
-$nb_height = $_GET['height'];
+<label for="width"></label>
+<input type="number" name="width" id="width">
 
+<input type="submit" value="Bâtissez votre maison!">
+</form>
+';
 
-// --------------- TRIANGLE ---------------
-
-$triangle = '';
-
-
-$lign = '';
-
-$lign_list = [];
-
-// Va servir à ajouter les lignes du triangle au tableau $lign_list, dans l'ordre
-$list_index = 0;
-
-$spaces = "";
+if (!isset($_GET['height']) || $_GET['height'] === '' || !isset($_GET['width']) || $_GET['width'] === '') {
+    echo 'Remplissez tous les champs !';
+} else {
 
 
-// Va servir à créer la surface du triangle, on part de la base et on enlève 1 de chaque côté, donc -2
-for ($surface = $nb_width; $surface >= 0 ; $surface -= 2) {
+    $nb_width = $_GET['width'];
+    $nb_height = $_GET['height'];
 
 
-    for ($i=0; $i < $surface; $i++) {
-        
-        // Ternaire : les côtés seront / & \ et le reste sera _
-        $char = ($i == ($surface - 1)) ? "\\" : (($i == 0) ? $char = "/" : "_");
+    // --------------- TRIANGLE ---------------
 
-        $lign .= $char;
-    }
-
-    // ajoute la ligne créée ci-dessus au tableau $lign_lists
-    $lign_list[$list_index] = $spaces . $lign;
-
-    // On reset la variable $lign pour le tour suivant
-    $lign = "";
+    $triangle = '';
 
 
-    $list_index += 1;
-    $spaces .= "&nbsp;";
+    $lign = '';
+
+    $lign_list = [];
+
+    // Va servir à ajouter les lignes du triangle au tableau $lign_list, dans l'ordre
+    $list_index = 0;
+
+    $spaces = "";
 
 
-
-    // echo "spaces => --$spaces--<br>";
-    
-}
+    // Va servir à créer la surface du triangle, on part de la base et on enlève 1 de chaque côté, donc -2
+    for ($surface = $nb_width; $surface >= 0 ; $surface -= 2) {
 
 
-// print_r($lign_list);
+        for ($i=0; $i < $surface; $i++) {
+            
+            // Ternaire : les côtés seront / & \ et le reste sera _
+            $char = ($i == ($surface - 1)) ? "\\" : (($i == 0) ? $char = "/" : "_");
 
-
-// dessine le triangle à l'aide du tableau
-foreach($lign_list as $lign) {
-    $triangle = $lign . '<br>' . $triangle;
-}
-
-
-// --------- RECTANGLE -----------
-
-$rectangle = '';
-
-for ($i = 0; $i < $nb_height; $i++) {
-
-    for ($j = 0; $j < $nb_width; $j++) {
-
-        // Si ce n'est pas un côté du rectangle
-        if ($j !== 0 && $j !== $nb_width-1) {
-
-            // Si ce n'est pas la base du rectangle, ajouter un spaces
-            if ($i !== $nb_height-1) {
-                $rectangle .= '&nbsp;';
-
-            // Sinon ajouter des underscores pour la base
-            } else {
-                $rectangle .= '_';
-            }
-
-        // Sinon c'est un côté du rectangle, et on ajoute des pipes
-        } else {
-            $rectangle .= '|';
+            $lign .= $char;
         }
+
+        // ajoute la ligne créée ci-dessus au tableau $lign_lists
+        $lign_list[$list_index] = $spaces . $lign;
+
+        // On reset la variable $lign pour le tour suivant
+        $lign = "";
+
+
+        $list_index += 1;
+        $spaces .= "&nbsp;";
+
+
+
+        // echo "spaces => --$spaces--<br>";
+        
     }
-    $rectangle .= '<br>';
+
+
+    // print_r($lign_list);
+
+
+    // dessine le triangle à l'aide du tableau
+    foreach($lign_list as $lign) {
+        $triangle = $lign . '<br>' . $triangle;
+    }
+
+
+    // --------- RECTANGLE -----------
+
+    $rectangle = '';
+
+    for ($i = 0; $i < $nb_height; $i++) {
+
+        for ($j = 0; $j < $nb_width; $j++) {
+
+            // Si ce n'est pas un côté du rectangle
+            if ($j !== 0 && $j !== $nb_width-1) {
+
+                // Si ce n'est pas la base du rectangle, ajouter un spaces
+                if ($i !== $nb_height-1) {
+                    $rectangle .= '&nbsp;';
+
+                // Sinon ajouter des underscores pour la base
+                } else {
+                    $rectangle .= '_';
+                }
+
+            // Sinon c'est un côté du rectangle, et on ajoute des pipes
+            } else {
+                $rectangle .= '|';
+            }
+        }
+        $rectangle .= '<br>';
+
+    }
+
+    echo '<div id="container"><pre>' . $triangle . $rectangle . '</pre></div>';
+
+
 
 }
 
 
 ?>
-
-<div id="container">
-    <?php echo '<pre>' . $triangle . $rectangle . '</pre>'; ?>
-</div>
-
-
-
