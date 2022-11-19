18be3eb32bf9bee9e8d443b032244933382a8298
felix-hauger <felix.hauger@laplateforme.io>
2022-11-19 09:16:35 +0100

    jour04, job07 - renommé variables en anglais & étoffé commentaires

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -13,7 +13,7 @@
 
 <?php
 
-$nb_width = 20;
+$nb_width = 40;
 $nb_height = 12;
 
 
@@ -22,50 +22,51 @@ $nb_height = 12;
 $triangle = '';
 
 
-$ligne = '';
+$lign = '';
 
-$liste_de_ligne = [];
+$lign_list = [];
 
+// Va servir à ajouter les lignes du triangle au tableau $lign_list, dans l'ordre
+$list_index = 0;
 
-$index_liste = 0;
-$espace = "";
+$spaces = "";
 
 
 // Va servir à créer la surface du triangle, on part de la base et on enlève 1 de chaque côté, donc -2
-for ($remplissage = $nb_width; $remplissage >= 0 ; $remplissage -= 2) {
+for ($surface = $nb_width; $surface >= 0 ; $surface -= 2) {
 
 
-    for ($i=0; $i < $remplissage; $i++) {
+    for ($i=0; $i < $surface; $i++) {
         
         // Ternaire : les côtés seront / & \ et le reste sera _
-        $char = ($i == ($remplissage - 1)) ? "\\" : (($i == 0) ? $char = "/" : "_");
+        $char = ($i == ($surface - 1)) ? "\\" : (($i == 0) ? $char = "/" : "_");
 
-        $ligne .= $char;
+        $lign .= $char;
     }
 
-    // ajoute la ligne créée ci-dessus au tableau $liste_de_lignes
-    $liste_de_ligne[$index_liste] = $espace . $ligne;
+    // ajoute la ligne créée ci-dessus au tableau $lign_lists
+    $lign_list[$list_index] = $spaces . $lign;
+
+    // On reset la variable $lign pour le tour suivant
+    $lign = "";
 
 
-    $ligne = "";
-
-
-    $index_liste += 1;
-    $espace .= "&nbsp;";
+    $list_index += 1;
+    $spaces .= "&nbsp;";
 
 
 
-    // echo "espace => --$espace--<br>";
+    // echo "spaces => --$spaces--<br>";
     
 }
 
 
-// print_r($liste_de_ligne);
+// print_r($lign_list);
 
 
 // dessine le triangle à l'aide du tableau
-foreach($liste_de_ligne as $ligne) {
-    $triangle = $ligne . '<br>' . $triangle;
+foreach($lign_list as $lign) {
+    $triangle = $lign . '<br>' . $triangle;
 }
 
 
@@ -80,7 +81,7 @@ for ($i = 0; $i < $nb_height; $i++) {
         // Si ce n'est pas un côté du rectangle
         if ($j !== 0 && $j !== $nb_width-1) {
 
-            // Si ce n'est pas la base du rectangle, ajouter un espace
+            // Si ce n'est pas la base du rectangle, ajouter un spaces
             if ($i !== $nb_height-1) {
                 $rectangle .= '&nbsp;';
 
