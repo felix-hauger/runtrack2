1a5e7dde2091f079c1ec65f8e50161834d953cab
felix-hauger <felix.hauger@laplateforme.io>
2022-11-28 12:26:58 +0100

    jour10, job03 - affiché les informations des etudiants de sexe féminin

---
+++ jour10/job03/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$id = mysqli_connect('localhost', 'root', '', 'jour09');
+
+$query = mysqli_query($id, 'select prenom, nom, naissance from etudiants where sexe = "femme"');
+
+$row = $query->fetch_assoc();
+
+
+echo '<table><thead><tr>';
+
+foreach ($row as $key => $value) {
+    echo '<th>' . $key . '</th>';
+}
+
+echo '</tr></thead><tbody>';
+
+while ($row != null) {
+    echo '<tr>';
+
+    foreach ($row as $value) {
+        echo '<td>' . $value . '</td>';
+    }
+
+    echo '<tr>';
+
+    $row = $query->fetch_assoc();
+}
+
+echo '</tbody></table>';
