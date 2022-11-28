00355694bb467b1b2f513208dd860780bc9a2d11
felix-hauger <felix.hauger@laplateforme.io>
2022-11-28 11:56:18 +0100

    jour10, job01 - affiché informations etudiants dans une table

---
+++ jour10/job01/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$id = mysqli_connect('localhost', 'root', '', 'jour09');
+
+$query = mysqli_query($id, 'select prenom, nom, naissance, sexe, email from etudiants');
+
+$row = $query->fetch_assoc();
+
+echo '<table><thead><tr>';
+
+
+foreach ($row as $key => $value) {
+    echo '<th>' . $key . '</th>';
+}
+
+echo '</tr></thead><tbody>';
+
+
+while ($row != null) {
+    echo '<tr>';
+    foreach ($row as $value) {
+        echo '<td>' . $value . '</td>';
+    }
+    echo '</tr>';
+    $row = $query->fetch_assoc();
+}
+
+echo '</tbody></table>';
