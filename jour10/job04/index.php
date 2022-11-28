70b744ce9e8edfba64e0d9cc4bcf5bd04f09ffb2
felix-hauger <felix.hauger@laplateforme.io>
2022-11-28 12:45:46 +0100

    jour10, job04 - affiché les informations des etudiants dont le prenom commence par T

---
+++ jour10/job04/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$id = mysqli_connect('localhost', 'root', '', 'jour09');
+
+$query = mysqli_query($id, 'select prenom, nom, naissance, sexe, email from etudiants where prenom like "T%"');
+
+$row = $query->fetch_assoc();
+
+
+echo '<table><thead><tr>';
+
+foreach ($row as $key => $value) {
+    echo '<th>' . $key . '</th>';
+}
+
+echo '</tr></thead><tbody>';
+
+while ($row != null) {
+    echo '<tr>';
+
+    foreach ($row as $value) {
+        echo '<td>' . $value . '</td>';
+    }
+
+    echo '</tr>';
+
+    $row = $query->fetch_assoc();
+}
+
+echo '</tbody></table>';
