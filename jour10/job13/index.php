1ab2bf2d4498d523b4a9f71faa32564aae9a9470
felix-hauger <felix.hauger@laplateforme.io>
2022-12-05 16:01:10 +0100

    jour10, job13 - affiché noms des salles et noms de leurs etages

---
+++ jour10/job13/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$id = mysqli_connect('localhost', 'root', '', 'jour09');
+
+$query = mysqli_query($id, "select salles.nom as NomSalle, etages.nom as NomEtage from salles inner join etages on salles.id_etage = etages.id");
+
+$row = $query->fetch_assoc();
+
+?>
+
+<table>
+    <thead>
+        <tr>
+        <?php foreach ($row as $key => $value): ?>
+            <th><?= $key ?></th>
+        <?php endforeach; ?>
+        </tr>
+    </thead>
+    <tbody>
+    <?php while ($row != null): ?>
+        <tr>
+        <?php foreach ($row as $value): ?>
+            <td><?= $value ?></td>
+        <?php endforeach; var_dump($row); $row = $query->fetch_assoc(); ?>
+        </tr>
+    <?php endwhile; ?>
+    </tbody>
+</table>
