d901ff5043aeaba88539eea084e66eb55d0319f7
felix-hauger <felix.hauger@laplateforme.io>
2022-12-05 15:10:17 +0100

    jour10, job12 - corrigé requête sql

--- jour10/job12/index.php
+++ jour10/job12/index.php
@@ -2,7 +2,7 @@
 
 $id = mysqli_connect('localhost', 'root', '', 'jour09');
 
-$query = mysqli_query($id, 'select prenom, nom, naissance from etudiants where naissance between 1998-01-01 and 2018-12-31');
+$query = mysqli_query($id, "select prenom, nom, naissance from etudiants where naissance between '1998-01-01' and '2018-12-31'");
 
 $row = $query->fetch_assoc();
 
